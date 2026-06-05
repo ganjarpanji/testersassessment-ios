0495f7024617c7afa068491439937b33c370505c
ganjarpanji <ushida.stranger@gmail.com>
2026-06-05 19:19:39 +0200

    Add UI test suite for landing screen using Page Object Model

---
+++ ReferenceiOSUITests/Constants/UITestConstants.swift
@@ -0,0 +1,32 @@
+//
+//  UITestConstants.swift
+//  ReferenceiOSUITests
+//
+//  Created by Ganjar Manggala on 04/06/2026.
+//  Copyright © 2026 ABN AMRO. All rights reserved.
+//
+
+import Foundation
+
+enum UIStatus: String {
+    case exist = "exists == true"
+    case hittable = "isHittable == 1"
+}
+
+enum TestTimeouts {
+    static let defaultTimeout: TimeInterval = 10.0
+}
+
+enum TextConstants {
+    static let hello: String = "Hello"
+    static let generateButton: String = "Button"
+    static let euroSymbol:String = "€"
+}
+
+enum SystemApps {
+    static let settingsApp: String = "com.apple.Preferences"
+}
+
+enum TestLocale {
+    static let US = "en_US"
+}
---
+++ ReferenceiOSUITests/Screens/BaseScreen.swift
@@ -0,0 +1,124 @@
+//
+//  BaseScreen.swift
+//  ReferenceiOSUITests
+//
+//  Created by Ganjar Manggala on 04/06/2026.
+//  Copyright © 2026 ABN AMRO. All rights reserved.
+//
+
+import Foundation
+import XCTest
+
+class BaseScreen {
+    
+    let app: XCUIApplication
+    init(app: XCUIApplication = XCUIApplication()) {
+        self.app = app
+    }
+    
+    // MARK: - Element Assertion
+    @discardableResult
+    func assertElementIsDisplayed(element: XCUIElement) -> Self {
+        expect(
+            element: element,
+            status: .exist
+        )
+        
+        return self
+    }
+    
+    @discardableResult
+    func assertElementIsHittable(element: XCUIElement) -> Self {
+        expect(
+            element: element,
+            status: .hittable,
+            message: "Validation failed: Element '\(element)' is not hittable"
+        )
+        
+        return self
+    }
+    
+    // MARK: - Label Assertion
+    @discardableResult
+    func assertLabelHasText(element: XCUIElement, text: String) -> Self {
+        expect(
+            element: element,
+            status: .exist
+        )
+        
+        XCTAssertEqual(
+            element.label,
+            text,
+            "Validation failed: Expected element label '\(element.label)' does not equal to '\(text)'"
+        )
+        
+        return self
+    }
+    
+    @discardableResult
+    func assertLabelHasNoText(element: XCUIElement, text: String) -> Self {
+        expect(
+            element: element,
+            status: .exist
+        )
+        
+        XCTAssertNotEqual(
+            element.label,
+            text,
+            "Validation failed: Expected element label '\(element.label)' equal to '\(text)'"
+        )
+        
+        return self
+    }
+    
+    @discardableResult
+    func assertLabelDoesNotContainText(element: XCUIElement, text: String) -> Self {
+        expect(
+            element: element,
+            status: .exist
+        )
+
+        XCTAssertFalse(
+            element.label.contains(text),
+            "Validation failed: Expected element label '\(element.label)' contains '\(text)'"
+        )
+
+        return self
+    }
+    
+    @discardableResult
+    func assertLabelContainsText(element: XCUIElement, text: String) -> Self {
+        expect(
+            element: element,
+            status: .exist
+        )
+        XCTAssertTrue(
+            element.label.contains(text),
+            "Validation failed: Expected element label '\(element.label)' does not contain '\(text)'"
+        )
+        
+        return self
+    }
+    
+    // MARK: - Waiting
+    @discardableResult
+    func expect(element: XCUIElement, status: UIStatus = .exist, timeout: TimeInterval = TestTimeouts.defaultTimeout, message: String? = nil) -> XCUIElement {
+        let elementExpectation = XCTNSPredicateExpectation(
+            predicate: NSPredicate(format: status.rawValue),
+            object: element
+        )
+        
+        let result = XCTWaiter.wait(
+            for: [elementExpectation],
+            timeout: timeout
+        )
+        
+        if result == .timedOut {
+            let defaultMessage = "Validation failed: Element '\(element)' does not match status '\(status.rawValue)'"
+            XCTFail(message ?? defaultMessage)
+        }
+        
+        return element
+    }
+    
+}
