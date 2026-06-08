03773f8d31be5737ceeb60c8bc26ca74a0279560
ganjarpanji <ushida.stranger@gmail.com>
2026-06-08 19:06:47 +0200

    Add snapshot test baseline

---
+++ ReferenceiOSSnapshotTests/Constants/SnapshotTestConstants.swift
@@ -0,0 +1,9 @@
+//
+//  SnapshotTestConstants.swift
+//  ReferenceiOSTests
+//
+//  Created by Ganjar Manggala on 08/06/2026.
+//  Copyright © 2026 ABN AMRO. All rights reserved.
+//
+
+import Foundation
