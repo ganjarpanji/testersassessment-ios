cef23c5f097475161e5e937af094f64c52756989
Ganjar Manggala <ganjar@backbase.com>
2026-08-21 03:16:05 +0200

    unitetest: add test for number in thousands, add test for attributify return original string

--- ReferenceiOSTests/CurrencyHelperTests.swift
+++ ReferenceiOSTests/CurrencyHelperTests.swift
@@ -10,25 +10,40 @@ import XCTest
 @testable import ReferenceiOS
 
 class CurrencyHelperTests: XCTestCase {
-    func testFormatRounding() {
+    func testFormat_WhenAmountHasThreeDecimalPlaces_ShouldRoundToTwoDecimalPlaces() {
         let number: NSNumber = 129.999
         let result = CurrencyHelper.format(amount: number)
         
         XCTAssertEqual(result, "€ 130,00")
     }
     
-    func testFormatRegular() {
+    func testFormat_WhenAmountIsRegularNumber_ShouldReturnCurrencyNumberWithDecimal() {
         let number: NSNumber = 122
         let result = CurrencyHelper.format(amount: number)
         
         XCTAssertEqual(result, "€ 122,00")
     }
     
-    func testFormatDecimals() {
+    func testFormat_WhenAmountIsDecimalNumber_ShouldReturnCurrencyUsingDecimalSeparator() {
         let number: NSNumber = 122.50
         let result = CurrencyHelper.format(amount: number)
         
         XCTAssertEqual(result, "€ 122,50")
     }
+    
+    func testFormat_WhenAmountHasThousands_ShouldReturnDutchThousandsSeparator() {
+        let number: NSNumber = 99999998
+        let result = CurrencyHelper.format(amount: number)
+        
+        XCTAssertEqual(result, "€ 99.999.998,00")
+    }
+    
+    func testAttributify_WhenInputIsFormattedAsCurrency_ShouldReturnOriginalString() {
+        let input = "€ 99.999.998,00"
+        let result = CurrencyHelper.attributify(amount: input)
+        
+        XCTAssertEqual(result.string, input)
+    }
+
 }
 
